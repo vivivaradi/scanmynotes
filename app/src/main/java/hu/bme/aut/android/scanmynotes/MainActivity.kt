0be64e93bb5ba1d955f8a1d354a5b058f2de2586
Vivi <varadivivien98@gmail.com>
2021-05-10 16:44:01 +0200

    set up toolbar in MainActivity

--- app/src/main/java/hu/bme/aut/android/scanmynotes/MainActivity.kt
+++ app/src/main/java/hu/bme/aut/android/scanmynotes/MainActivity.kt
@@ -1,32 +1,18 @@
 package hu.bme.aut.android.scanmynotes
 
-import android.content.Intent
-import android.graphics.Bitmap
-import android.media.NotProvisionedException
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.MediaStore
-import androidx.lifecycle.MutableLiveData
-import com.google.api.client.extensions.android.json.AndroidJsonFactory
-import com.google.api.client.http.javanet.NetHttpTransport
-import com.google.api.services.vision.v1.Vision
-import com.google.api.services.vision.v1.VisionRequestInitializer
-import com.google.api.services.vision.v1.model.AnnotateImageRequest
-import com.google.api.services.vision.v1.model.BatchAnnotateImagesRequest
-import com.google.api.services.vision.v1.model.Feature
-import com.google.api.services.vision.v1.model.Image
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import java.io.ByteArrayOutputStream
-import java.util.*
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        toolbar.title = title
 
     }
 
