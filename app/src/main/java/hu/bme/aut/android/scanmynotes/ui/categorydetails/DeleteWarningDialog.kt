f277c0e35ad9d98155c26647512fd19f4b8e83f3
Vivi <varadivivien98@gmail.com>
2021-11-17 01:44:48 +0100

    extract string resources

--- app/src/main/java/hu/bme/aut/android/scanmynotes/ui/categorydetails/DeleteWarningDialog.kt
+++ app/src/main/java/hu/bme/aut/android/scanmynotes/ui/categorydetails/DeleteWarningDialog.kt
@@ -14,8 +14,8 @@ class DeleteWarningDialog: DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let { activity ->
             val builder = AlertDialog.Builder(activity)
-            builder.setTitle("Delete category?")
-                .setMessage("This will delete all categories and notes in this category.")
+            builder.setTitle(getString(R.string.delete_confirm_dialog_title))
+                .setMessage(getString(R.string.delete_confirm_dialog_message))
                 .setPositiveButton(getString(R.string.warning_dialog_confirm_button_text), DialogInterface.OnClickListener { dialogInterface, id ->
                     parentFragmentManager.setFragmentResult(getString(R.string.delete_dialog_result_requestkey), bundleOf())
                 })
